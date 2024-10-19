134e0f720ac82a14ea6364bfbeb8d1958c685ae7
Zyadwalid1 <zyadwallo.77@gmail.com>
2024-10-19 21:58:15 +0300

    first commit

---
+++ src/pages/MobileRedicert/MobilePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Mobile.css'
+
+const Mobile = () => {
+  return (
+    <div className='mobile-container'>
+      <h1 className='wops'>Whooops!</h1>
+      <br />
+      <h1>EN</h1>
+      <p>Dear visitor, this website is currently not optimized for mobile devices. Please use a desktop to log in.</p>
+      <h1>AR</h1>
+      <p>عزيزي الزائر ، لم يتم تحسين هذا الموقع حاليًا للأجهزة المحمولة. يرجى استخدام سطح المكتب لتسجيل الدخول.</p>
+    </div>
+  )
+}
+
+export default Mobile
